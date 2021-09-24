62172b24d40accf4e6e2aff94e88424910d84464
김민수 <alstntorl@naver.com>
2021-09-24 23:13:06 +0900

    6강 공부중

--- 응용/6강/src/script.js
+++ 응용/6강/src/script.js
@@ -1,45 +1,59 @@
 import './style.css'
-import * as THREE from 'three' 
+import * as THREE from 'three'
 import gsap from 'gsap'
 
-// Canvas 
+/**
+ * Base
+ */
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// 화면 
-const Scene = new THREE.Scene()
- 
-// Base 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff000 })
-const mesh = new THREE.Mesh(geometry, material)
-Scene.add(mesh)
+// Scene
+const scene = new THREE.Scene()
 
-// 크기
+/**
+ * Base
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
 const sizes = {
-    width: 900, 
-    height: 700
+    width: 900,
+    height: 600
 }
 
-
-// 카메라 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) 
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-Scene.add(camera)
+scene.add(camera)
 
-// 렌더링.
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas 
+    canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height) 
+renderer.setSize(sizes.width, sizes.height)
 
-// Animate 
-gsap.to(mesh.position, { duration: 1, delay: 1, x : 2 })
+/**
+ * Animate
+ */
+gsap.to(mesh.position, { duration: 2, delay: 1, x: 2 })
 
-const tick = () => {
+const tick = () =>
+{
     // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame 
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
+
 tick()
